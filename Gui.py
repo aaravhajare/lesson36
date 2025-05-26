07e3c3c518bcb14436b2982729fd96d39815ae00
aaravhajare <hajareaarav66@gmail.com>
2025-05-26 09:29:25 +0530

    Gui

---
+++ Gui.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+# Create the main window
+window = Tk()
+
+# Set the window title
+window.title("My window")
+
+# Set the window size (width x height)
+window.geometry("400x300")
+
+# Run the Tkinter event loop
+window.mainloop()
